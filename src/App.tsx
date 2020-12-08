ca6e54635a204b92dd35fb35b7629401fde27fc4
David Vendel <33971649+justdvl@users.noreply.github.com>
2020-12-08 17:57:14 +0100

    headline

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import Headline from './components/Headline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>Hello!</h1>
-      </header>
-    </div>
+    <><Headline />
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello!</h1>
+            </header>
+        </div>
+    </>
   );
 }
 
