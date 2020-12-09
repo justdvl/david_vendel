dfdc41ceea62544796cf50d3cb11f71c83d8d6b3
David Vendel <33971649+justdvl@users.noreply.github.com>
2020-12-09 21:39:50 +0100

    link wont open full path

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,10 +9,10 @@ return (
             <h1>Hello!</h1>
         </div>
         <div className="half">
-            <Link to="/cube_game"><h3>Cube Game</h3></Link>
-            <Link to="/dve_bodky"><h3>Dve Bodky</h3></Link>
-            <Link to="/task_manager"><h3>Task Manager</h3></Link>
-            <Link to="/multilingo"><h3>Multilingo</h3></Link>
+            <h3 onClick={() => {window.location.href="https://davidvendel.com/cube_game"}}>Cube Game</h3>
+            <h3 onClick={() => {window.location.href="https://davidvendel.com/dve_bodky"}}>Dve Bodky</h3>
+            <h3 onClick={() => {window.location.href="https://davidvendel.com/task_manager"}}>Task Manager</h3>
+            <h3 onClick={() => {window.location.href="https://davidvendel.com/multilingo"}}>Multilingo</h3>
         </div>
     </header>
     </div>
