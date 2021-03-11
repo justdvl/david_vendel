ef5bfc4572bf1bfacebf60f1cedba751a868feaf
David Vendel <33971649+justdvl@users.noreply.github.com>
2021-03-11 21:16:57 +0100

    bird

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,22 +1,56 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 const MainPage = () => {
-
-return (
-    <div className="App">
-    <header className="App-header">
-        <div className="half">
-            <h1>Hello!</h1>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="half">
+                    <h1>Hello!</h1>
+                </div>
+                <div className="half">
+                    <h3
+                        onClick={() => {
+                            window.location.href =
+                                'https://davidvendel.com/cube_game';
+                        }}
+                    >
+                        Cube Game
+                    </h3>
+                    <h3
+                        onClick={() => {
+                            window.location.href =
+                                'https://davidvendel.com/bird';
+                        }}
+                    >
+                        Simple Bird
+                    </h3>
+                    <h3
+                        onClick={() => {
+                            window.location.href =
+                                'https://davidvendel.com/dve_bodky';
+                        }}
+                    >
+                        Dve Bodky
+                    </h3>
+                    <h3
+                        onClick={() => {
+                            window.location.href =
+                                'https://davidvendel.com/task_manager';
+                        }}
+                    >
+                        Task Manager
+                    </h3>
+                    <h3
+                        onClick={() => {
+                            window.location.href = 'http://multilingo.online';
+                        }}
+                    >
+                        Multilingo
+                    </h3>
+                </div>
+            </header>
         </div>
-        <div className="half">
-            <h3 onClick={() => {window.location.href="https://davidvendel.com/cube_game"}}>Cube Game</h3>
-            <h3 onClick={() => {window.location.href="https://davidvendel.com/dve_bodky"}}>Dve Bodky</h3>
-            <h3 onClick={() => {window.location.href="https://davidvendel.com/task_manager"}}>Task Manager</h3>
-            <h3 onClick={() => {window.location.href="http://multilingo.online"}}>Multilingo</h3>
-        </div>
-    </header>
-    </div>
-    )
-}
+    );
+};
 
-export default MainPage
+export default MainPage;
